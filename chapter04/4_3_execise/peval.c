8d20c951e9a72d1b6b111f0de66c02ccb811116a
hmsjwzb <hmsjwzb@qq.com>
2022-01-07 15:11:48 +0800

    [AIC] postfix-expression and Infix-to-postfix
    	Include substract and div

---
+++ chapter04/4_3_execise/peval.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "Item.h"
+#include "STACK.h"
+
+int main(int argc, char *argv[])
+{
+	char *a = argv[1]; int i, N = strlen(a);
+	STACKinit(N);
+	for (i = 0; i < N; i++)
+	{
+		if (a[i] == '+')
+			STACKpush(STACKpop() + STACKpop());
+		if (a[i] == '*')
+			STACKpush(STACKpop() * STACKpop());
+		if ((a[i] >= '0') && (a[i] <= '9'))
+			STACKinit(0);
+		while ((a[i] >= '0') && (a[i] <= '9'))
+			STACKpush(0);
+	}
+	printf("%d\n", STACKpop());
+}
