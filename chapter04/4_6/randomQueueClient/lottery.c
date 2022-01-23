a042c7b1c4e576dfdb0991e80eb96d0653c17736
hmsjwzb <hmsjwzb@qq.com>
2022-01-23 15:23:12 +0800

    [AIC] lottery
    	Exercise 4.44

---
+++ chapter04/4_6/randomQueueClient/lottery.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Item.h"
+#include "randomQueue.h"
+
+int main(int argc, char *argv[])
+{
+	int i;
+	randomQueueInit(1);
+	for (i = 0; i < 100; i++) {
+		randomQueueEnqueue(i);
+	}
+
+	for (i = 0; i < 5; i++) {
+		printf("%d ", randomQueueDequeue());
+	}
+	printf("\n");
+	return 0;
+}
