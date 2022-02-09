92f9cc5ca937053d41f3f6567a8097af56d133c2
hmsjwzb <hmsjwzb@qq.com>
2022-02-09 15:10:38 +0800

    [AIC] forget old item
    	Exercise 4.49

---
+++ chapter04/4_7/Exercises/forget_old/stackArray.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "Item.h"
+#include "STACK.h"
+static Item *s;
+static int N;
+
+void STACKinit(int maxN)
+{
+	s = malloc(maxN * sizeof(Item));
+	N = 0;
+}
+
+int STACKempty()
+{
+	return N == 0;
+}
+
+void STACKpush(Item item)
+{
+	int i, j;
+	for (i = 0; i < N; i++) {
+		if (s[i] == item)
+			break;
+	}
+	for (j = i; j < N - 1; j++) {
+		s[j] = s[j+1];
+	}
+	N -= 1;
+	s[N++] = item;
+}
+
+Item STACKpop()
+{
+	return s[--N];
+}
