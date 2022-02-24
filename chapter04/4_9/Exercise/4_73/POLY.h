d072ce04f2a301296fc878941a4f867cf76a26d9
hmsjwzb <hmsjwzb@qq.com>
2022-02-24 09:30:39 +0800

    [AIC] polynomial integral & diff
    	exercise 4.73

---
+++ chapter04/4_9/Exercise/4_73/POLY.h
@@ -0,0 +1,8 @@
+typedef struct poly *Poly;
+void showPOLY(Poly);
+Poly POLYterm(int, int);
+Poly POLYadd(Poly, Poly);
+Poly POLYmult(Poly, Poly);
+float POLYeval(Poly, float);
+Poly POLYintegral(Poly);
+Poly POLYdiff(Poly);
