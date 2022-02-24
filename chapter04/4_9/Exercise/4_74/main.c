83917bbc95911cc3e2e3cf1b0e4bd22eb7631d2b
hmsjwzb <hmsjwzb@qq.com>
2022-02-24 10:26:09 +0800

    [AIC] ignore greater or equal than M
    	Exercise 4.74

---
+++ chapter04/4_9/Exercise/4_74/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "POLY.h"
+
+int main(int argc, char *argv[])
+{
+	Poly t, p, q, x, r;
+	t = POLYadd(POLYterm(1, 1), POLYterm(1, 0));
+	x = t;
+
+	p = POLYadd(POLYterm(100, 100), POLYterm(50, 50));
+	p = POLYadd(p, POLYterm(25, 25));
+	p = POLYadd(p, POLYterm(1, 99));
+	p = POLYadd(p, POLYterm(1, 0));
+
+	q = POLYadd(POLYterm(1000, 1000), POLYterm(500, 500));
+	q = POLYadd(q, POLYterm(250, 250));
+	q = POLYadd(q, POLYterm(99, 99));
+	q = POLYadd(q, POLYterm(49, 49));
+	q = POLYadd(q, POLYterm(24, 24));
+	r = POLYadd(p, q);
+	//showPOLY(r);
+
+	r = POLYignore(r, 100);
+	showPOLY(r);
+	/*for (int i = 0; i < 5; i++) {
+		x = POLYmult(t, x);
+		showPOLY(x);
+	}*/
+
+}
