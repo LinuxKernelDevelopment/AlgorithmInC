cf8122d5a63b5b06fc6b6d13c9d428092dcbf587
hmsjwzb <hmsjwzb@qq.com>
2022-03-30 15:06:05 +0800

    [AIC] recursive reverse link list
    	Exercise 5.15

---
+++ chapter05/5.1/Exercises/listreverse/link.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "item.h"
+
+typedef struct node *link;
+struct node { int item; link next; };
+
+int count(link x)
+{
+	if (x == NULL)
+		return 0;
+	return 1 + count(x->next);
+}
+
+void traverse(link h, void (*visit)(link))
+{
+	if (h == NULL)
+		return;
+	(*visit)(h);
+	traverse(h->next, visit);
+}
+
+void traverseR(link h, void (*visit)(link))
+{
+	if (h == NULL)
+		return;
+	traverseR(h->next, visit);
+	(*visit)(h);
+}
+
+link delete(link x, Item v)
+{
+	if (x == NULL)
+		return NULL;
+	if (x->item == v)
+	{
+		link t = x->next;
+		free(x);
+		return t;
+	}
+	x->next = delete(x->next, v);
+	return x;
+}
+
+void deleteLast(link x)
+{
+	if (x->next && !x->next->next) {
+		x->next = NULL;
+		return;
+	}
+
+	deleteLast(x->next);
+}
+
+void visit(link x)
+{
+	printf("%d\t", x->item);
+}
+
+static link prev = NULL;
+link reverse(link t)
+{
+	if (!t->next) {
+		t->next = prev;
+		prev = NULL;
+		return t;
+	}
+
+	link next = t->next;
+	t->next = prev;
+	prev = t;
+	return reverse(next);
+}
---
+++ chapter05/5.1/Exercises/listreverse/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "item.h"
+#include "link.h"
+
+int main(void)
+{
+	int i;
+	link t = malloc(sizeof *t), x = t;
+	t->item = 1; t->next = NULL;
+	for (i = 2; i <= 10; i++)
+	{
+		x = (x->next = malloc(sizeof *x));
+		x->item = i; x->next = NULL;
+	}
+	traverse(t, visit);
+	printf("\n");
+	deleteLast(t);
+	traverse(t, visit);
+	printf("\n");
+	link t2 = reverse(t);
+	traverse(t2, visit);
+	printf("\n");
+	return 0;
+}
