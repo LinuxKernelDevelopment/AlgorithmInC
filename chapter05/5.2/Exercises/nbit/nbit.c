99b1ce0f3af83cfc2da50be1f5f65eeadb135a3b
hmsjwzb <hmsjwzb@zoho.com>
2022-07-21 04:40:06 -0400

    [AIC] nbit print

--- chapter05/5.2/Exercises/nbit/nbit.c
+++ chapter05/5.2/Exercises/nbit/nbit.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 char **nbit_arr;
 void init_nbit(int nbit)
 {
+	int i;
 	int nentry = 1 << nbit;
 
 	nbit_arr = malloc(sizeof(char *) * nentry);
@@ -13,27 +15,39 @@ void init_nbit(int nbit)
 	}
 }
 
-void fillnbit(int nbit, int sn)
+void dump_nbit(int nentry)
 {
-	int nentry = 1 << nbit, i;
+	int i;
 
-	for (i = 0; i < (1 << (nbit - 1)); i++) {
-		nbit_arr[i][sn] = '0';
-	}
+	for (i = 0; i < nentry; i++)
+		printf("%s\n", nbit_arr[i]);
+}
 
-	for (i = (1 << (nbit - 1)); i < (1 << nbit); i++) {
-		nbit_arr[i][sn] = '1';
-	}
-
-	fillnbit(nbit - 1, sn + 1);
-
-
-int main(int argc, char *argv)
+void fillnbit(int start, int end, int bitPos)
 {
-	int nbit = argv[1];
+	int i, j, mid;
+	if (start + 1 == end)
+		return;
+
+	mid = (start + end) / 2;
+	for (i = start; i < mid; i++)
+		nbit_arr[i][bitPos] = '0';
+
+	for (j = mid; j < end; j ++)
+		nbit_arr[j][bitPos] = '1';
+
+	fillnbit(start, mid, bitPos + 1);
+	fillnbit(mid, end, bitPos + 1);
+}
+
+int main(int argc, char **argv)
+{
+	int nbit = atoi(argv[1]);
+	int nentry = 1 << nbit;
 
 	init_nbit(nbit);
-	fillnbit(nbit, 0);
+	fillnbit(0, nentry, 0);
+	dump_nbit(nentry);
 
 	return 0;
 }
